b9a1f1a861850d583b5bd998c2459f3eae35fc5d
Stanisław Kaczmarek <stanislaw.kaczmarek@student.put.poznan.pl>
2021-01-07 19:56:42 +0100

    Gumbo integrated

--- server/website/website.cc
+++ server/website/website.cc
@@ -1,5 +1,7 @@
 #include "website/website.h"
 
+#include <gumbo.h>
+
 namespace pseudogoogle {
 
 Website::Website(const std::string& url) {
@@ -8,6 +10,10 @@ Website::Website(const std::string& url) {
   // verify content-type
   // parse using gumbo
   // add words to word_counter_ and link to links
+
+  GumboOutput* output = gumbo_parse("<h1>Hello, World!</h1>");
+  // Do stuff with output->root
+  gumbo_destroy_output(&kGumboDefaultOptions, output);
 }
 
 }  // namespace pseudogoogle
