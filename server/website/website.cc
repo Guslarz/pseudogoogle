c0aa35a0930d2ad8932d25c0c434e924dd1dd95c
Stanisław Kaczmarek <stanislaw.kaczmarek@student.put.poznan.pl>
2021-01-07 20:27:38 +0100

    Replace gumbo with myhtml

--- server/website/website.cc
+++ server/website/website.cc
@@ -1,6 +1,6 @@
 #include "website/website.h"
 
-#include <gumbo.h>
+#include <myhtml/api.h>
 
 namespace pseudogoogle {
 
@@ -10,10 +10,30 @@ Website::Website(const std::string& url) {
   // verify content-type
   // parse using gumbo
   // add words to word_counter_ and link to links
+  char html[] = "<div><span>HTML</span></div>";
 
-  GumboOutput* output = gumbo_parse("<h1>Hello, World!</h1>");
-  // Do stuff with output->root
-  gumbo_destroy_output(&kGumboDefaultOptions, output);
+  // basic init
+  myhtml_t* myhtml = myhtml_create();
+  myhtml_init(myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
+
+  // first tree init
+  myhtml_tree_t* tree = myhtml_tree_create();
+  myhtml_tree_init(tree, myhtml);
+
+  // parse html
+  myhtml_parse(tree, MyENCODING_UTF_8, html, strlen(html));
+
+  // print result
+  // or see serialization function with callback:
+  // myhtml_serialization_tree_callback
+  mycore_string_raw_t str = {0};
+  myhtml_serialization_tree_buffer(myhtml_tree_get_document(tree), &str);
+  printf("%s\n", str.data);
+
+  // release resources
+  mycore_string_raw_destroy(&str, false);
+  myhtml_tree_destroy(tree);
+  myhtml_destroy(myhtml);
 }
 
 }  // namespace pseudogoogle
